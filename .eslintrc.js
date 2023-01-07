9f10ddcd70573bf7c25019550be6e30c984b08a1
Ricardo Dantas <86023+ricardodantas@users.noreply.github.com>
2023-01-07 02:50:33 +0100

    add lint support

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jest/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+    },
+}
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,12 @@
-export const TWITTER_CONSUMER_KEY = process.env["TWITTER_CONSUMER_KEY"] ?? '';
-export const TWITTER_CONSUMER_SECRET = process.env["TWITTER_CONSUMER_SECRET"] ?? '';
-export const TWITTER_ACCESS_TOKEN_KEY =  process.env["TWITTER_ACCESS_TOKEN_KEY"] ?? '';
-export const TWITTER_ACCESS_TOKEN_SECRET =  process.env["TWITTER_ACCESS_TOKEN_SECRET"] ?? '';
+export const TWITTER_CONSUMER_KEY = process.env['TWITTER_CONSUMER_KEY'] ?? ''
+export const TWITTER_CONSUMER_SECRET =
+  process.env['TWITTER_CONSUMER_SECRET'] ?? ''
+export const TWITTER_ACCESS_TOKEN_KEY =
+  process.env['TWITTER_ACCESS_TOKEN_KEY'] ?? ''
+export const TWITTER_ACCESS_TOKEN_SECRET =
+  process.env['TWITTER_ACCESS_TOKEN_SECRET'] ?? ''
 
-export const MASTODON_ACCESS_TOKEN = process.env["MASTODON_ACCESS_TOKEN"] ?? '';
-export const MASTODON_URL = process.env["MASTODON_URL"] ?? '';
+export const MASTODON_ACCESS_TOKEN = process.env['MASTODON_ACCESS_TOKEN'] ?? ''
+export const MASTODON_URL = process.env['MASTODON_URL'] ?? ''
 
 export const CACHE_FILE_NAME = 'posted-tweet.json'
