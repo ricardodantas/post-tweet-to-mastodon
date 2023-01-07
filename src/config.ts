df4218e2bbde9cda19e211e200e28420ee6d96af
Ricardo Dantas <86023+ricardodantas@users.noreply.github.com>
2023-01-07 01:15:01 +0100

    start

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const TWITTER_CONSUMER_KEY = process.env["TWITTER_CONSUMER_KEY"] ?? '';
+export const TWITTER_CONSUMER_SECRET = process.env["TWITTER_CONSUMER_SECRET"] ?? '';
+export const TWITTER_ACCESS_TOKEN_KEY =  process.env["TWITTER_ACCESS_TOKEN_KEY"] ?? '';
+export const TWITTER_ACCESS_TOKEN_SECRET =  process.env["TWITTER_ACCESS_TOKEN_SECRET"] ?? '';
+
+export const MASTODON_ACCESS_TOKEN = process.env["MASTODON_ACCESS_TOKEN"] ?? '';
+export const MASTODON_URL = process.env["MASTODON_URL"] ?? '';
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import Twit from 'twit'
+import { login } from 'masto';
+import * as dotenv from 'dotenv';
+import { writeFileSync, readFileSync, existsSync } from 'fs'
+import chalk from 'chalk';
+
+dotenv.config()
+
+import {
+  TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN_KEY,
+  TWITTER_ACCESS_TOKEN_SECRET, MASTODON_URL, MASTODON_ACCESS_TOKEN
+} from './config';
+
+
+const CACHE_FILE_NAME = 'posted_tweet.json'
+
+async function fetchLatestTweet() {
+  const client = new Twit({
+    consumer_key: TWITTER_CONSUMER_KEY,
+    consumer_secret: TWITTER_CONSUMER_SECRET,
+    access_token: TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  const params = { count: 1, tweet_mode: 'extended' };
+
+  const { data } = await client.get('statuses/user_timeline', params);
+  const tweet = (data as any)[0].full_text
+  const tweetId = (data as any)[0].id_str
+  console.log(chalk.blue(`Tweet fetched: ${tweet}`));
+  return {tweetId, tweet};
+}
+
+async function postToMastodon(text: string) {
+  const masto = await login({
+    url: MASTODON_URL,
+    accessToken: MASTODON_ACCESS_TOKEN,
+  });
+
+  await masto.v1.statuses.create({
+    status: text,
+    visibility: 'public',
+  })
+  console.log(chalk.green('Tweet posted to Mastodon successfully!'));
+}
+
+
+
+function getLatestPostedTweetId(): string | null {
+  if (!existsSync(CACHE_FILE_NAME)) {
+    return null
+  }
+  const unformattedJson = readFileSync(CACHE_FILE_NAME);
+  const parsedJson = JSON.parse(unformattedJson?.toString());
+  return parsedJson?.tweetId
+}
+
+function storePostedTweetId(tweetId:string) {
+  writeFileSync(CACHE_FILE_NAME, JSON.stringify({ tweetId }, null, 2));
+}
+
+async function main() {
+  const latestPostedTweet = getLatestPostedTweetId();
+  const { tweet, tweetId } = await fetchLatestTweet();
+  if (latestPostedTweet === tweetId) {
+    console.log(chalk.yellow('Skipping this tweet once it was already posted.'));
+    return
+  }
+  await postToMastodon(tweet);
+  storePostedTweetId(tweetId)
+}
+
+main();
